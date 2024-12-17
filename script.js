8dfb2814e57f60a0633ae9966d953a2e685e72a8
KaushiKanagarajan <kaushikanagarajan57@gmail.com>
2024-12-17 13:34:58 +0530

    first responsive web page

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => 
+  {
+    const navbar = document.querySelector('.navbar');
+  
+    window.addEventListener('scroll', () => 
+      {
+        if (window.scrollY > 50) 
+          {
+            navbar.classList.add('scrolled');
+          } 
+        else 
+          {
+            navbar.classList.remove('scrolled');
+          }
+      });
+  });
+  
